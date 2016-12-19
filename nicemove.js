f3895b8ccb895cb256f4792afa0e8e46d0f396dc
Chunlong <longbill.cn@gmail.com>
2016-12-19 20:47:25 +0800

    fix bug

--- nicemove.js
+++ nicemove.js
@@ -1,8 +1,7 @@
 
 (function() {
 
-var sys = function()
-{
+var sys = function() {
 	var ua = navigator.userAgent,
 		b = ua.match(/iPhone/i),
 		c = ua.match(/iPad/i),
@@ -54,29 +53,23 @@ var sys = function()
 /**
  * useful utilities
  */
-var utils = function()
-{
+var utils = function() {
 	var prefix, transformName, transitionName;
 	
-	function trans(el, v)
-	{
+	function trans(el, v) {
 		el.style[transformName] = v;
 	}
 
-	(function(html)
-	{
-		var g =
-		[
+	(function(html) {
+		var g = [
 			["transform", "", "transition"],
 			["WebkitTransform", "-webkit-", "WebkitTransition"],
 			["MozTransform", "-moz-", "MozTransition"],
 			["OTransform", "-o-", "OTransition"],
 			["msTransform", "-ms-", "msTransition"]
 		];
-		for (var i = g.length; i--;)
-		{
-			if ("undefined" != typeof html.style[g[i][0]])
-			{
+		for (var i = g.length; i--;) {
+			if ("undefined" != typeof html.style[g[i][0]]) {
 				var f = g[i];
 				transformName = f[0];
 				prefix = f[1];
@@ -99,8 +92,7 @@ var utils = function()
 		 * set css translate property, use 3d translate if possible
 		 * transform2(element, horizontal px, vertical px, <scale>)
 		 */
-		transform2: function(el, x, y, scale)
-		{
+		transform2: function(el, x, y, scale) {
 			var v = sys.supports.translate3d ? "translate3d(" + x + "px," + y + "px,0px)" : "translate(" + x + "px," + y + "px)";
 			if (scale) v += " scale(" + scale + ")";
 			trans(el, v);
@@ -112,8 +104,7 @@ var utils = function()
 		 * example:
 		 * 		transition(el, 'opacity', 'ease .3s')
 		 */
-		transition: function(el, key, animation, delay)
-		{
+		transition: function(el, key, animation, delay) {
 			if ("transform" == key) key = prefix + key;
 			el.style[transitionName] = key + " " + animation;
 			if (delay) el.style[prefix + "transition-delay"] = delay + "ms";
@@ -122,8 +113,7 @@ var utils = function()
 		/**
 		 * clear css transition property
 		 */
-		resetTransition: function(el)
-		{
+		resetTransition: function(el) {
 			el.style[transitionName] = '';  // "all 0.0001ms linear"
 			el.style[prefix + "transition-delay"] = "0";
 		},
@@ -131,12 +121,10 @@ var utils = function()
 		/**
 		 * delegate events
 		 */
-		delegate: function(a, b)
-		{
+		delegate: function(a, b) {
 			var c = Array.prototype.slice.call(arguments);
 			c.splice(0, 2);
-			return function()
-			{
+			return function() {
 				b.apply(a, c.length ? c : Array.prototype.slice.call(arguments));
 			}
 		}
@@ -144,19 +132,15 @@ var utils = function()
 }();
 
 
-var math = function()
-{
+var math = function() {
 	return {
-		clamp: function(a, b, c)
-		{
+		clamp: function(a, b, c) {
 			return a > c ? c : a < b ? b : a;
 		},
-		distance: function(x1, y1, x2, y2)
-		{
+		distance: function(x1, y1, x2, y2) {
 			return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 		},
-		addPointOnLine: function(x, y, c, k, g)
-		{
+		addPointOnLine: function(x, y, c, k, g) {
 			return {
 				x: c + (x - c) * g,
 				y: k + (y - k) * g
@@ -168,14 +152,12 @@ var math = function()
 /**
  * get the proper next frame function
  */
-var requestAnimFrame = function()
-{
+var requestAnimFrame = function() {
 	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(a, b) { window.setTimeout(a, 1E3 / 60); }
 }();
 
 
-function RBand()
-{
+function RBand() {
 	this.max = this.min = this.contPos = this.v = this.p = 0;
 	this.isBouncy = this.isMoving = false;
 	this.vObj = 
@@ -186,50 +168,39 @@ function RBand()
 	}
 };
 
-RBand.prototype.set = function(a)
-{
+RBand.prototype.set = function(a) {
 	this.p = a;
 	this.v = 0
 };
 
-RBand.prototype.setBounds = function(a, b)
-{
+RBand.prototype.setBounds = function(a, b) {
 	this.min = a;
 	this.max = b
 };
 
-RBand.prototype.update = function(isDraggingPos, dragX, targetX)
-{
-	if (isDraggingPos)
-	{
+RBand.prototype.update = function(isDraggingPos, dragX, targetX) {
+	if (isDraggingPos) {
 		this.v = dragX - this.p;
 		this.p = dragX;
 		if (this.contPos < this.min || this.contPos > this.max) this.v *= 0.5;
 		this.contPos += this.v;
 		this.isMoving = true
-	}
-	else
-	{
+	} else {
 		this.isMoving = 0.1 < Math.abs(this.v);
 		this.v *= 0.97;
 		var a = this.contPos + this.v;
 		var b = this.isBouncy ? a : this.contPos;
-		if (void 0 !== targetX)
-		{
+		if (void 0 !== targetX) {
 			var c = this.velo(b, targetX, this.v);
 			a = c.pos;
 			this.v = c.v;
 			this.isMoving = c.isMoving;
-		}
-		else if (a < this.min)
-		{
+		} else if (a < this.min) {
 			var c = this.velo(b, this.min, this.v);
 			a = c.pos;
 			this.v = c.v;
 			this.isMoving = c.isMoving;
-		}
-		else if (a > this.max)
-		{
+		} else if (a > this.max) {
 			var c = this.velo(b, this.max, this.v);
 			a = c.pos;
 			this.v = c.v;
@@ -240,18 +211,14 @@ RBand.prototype.update = function(isDraggingPos, dragX, targetX)
 	return this.contPos
 };
 
-RBand.prototype.velo = function(a, b, c)
-{
+RBand.prototype.velo = function(a, b, c) {
 	this.vObj.isMoving = 0.5 < Math.abs(b - a) || 0.5 < Math.abs(c);
-	if (this.vObj.isMoving)
-	{
+	if (this.vObj.isMoving) {
 		c = 0.74 * (c + 0.04 * (b - a));
 		a += c;
 		this.vObj.pos = a;
 		this.vObj.v = c;
-	}
-	else
-	{
+	} else {
 		this.vObj.v = 0;
 		this.vObj.pos = b;
 	}
@@ -260,8 +227,7 @@ RBand.prototype.velo = function(a, b, c)
 
 
 
-function _nicemove(el, scrollHorizontal, scrollVertical, options)
-{
+function _nicemove(el, scrollHorizontal, scrollVertical, options) {
 	this.isRunning = false;
 	this.holder = this.content = null;
 	this.minY = this.minX = this.contentY = this.contentX = 0;
@@ -305,15 +271,13 @@ function _nicemove(el, scrollHorizontal, scrollVertical, options)
 	sys.is.android && (this.timeThresh = 100)
 };
 
-_nicemove.prototype.activate = function(a)
-{
+_nicemove.prototype.activate = function(a) {
 	this.handle[a ? "addEventListener" : "removeEventListener"](sys.supports.touch ? "touchstart" : "mousedown", this.startDel, false);
 	this.isActive = a;
 	this.isDraggingPos && this.dragEnd(this.dragX, this.dragY, {})
 };
 
-_nicemove.prototype.remove = function()
-{
+_nicemove.prototype.remove = function() {
 	this.pause();
 	this.activate(false);
 	this.listenForMoveAndEnd(false);
@@ -321,8 +285,7 @@ _nicemove.prototype.remove = function()
 	this.content = this.holder = this.onPageChanged = null
 };
 
-_nicemove.prototype.slideTo = function(x, y, ms, easing)
-{
+_nicemove.prototype.slideTo = function(x, y, ms, easing) {
 	void 0 == ms && (ms = 350);
 	utils.resetTransition(this.content);
 	utils.transform2(this.content, -x, -y);
@@ -330,43 +293,36 @@ _nicemove.prototype.slideTo = function(x, y, ms, easing)
 	var e = this;
 	setTimeout(function(){ e.pause(); }, 0);
 	clearTimeout(this.slideCompleteTimeout);
-	this.slideCompleteTimeout = setTimeout(function()
-	{
+	this.slideCompleteTimeout = setTimeout(function() {
 		utils.resetTransition(e.content)
 	}, ms + 1);
 	this.doScrollX && (this.rBandX.contPos = this.contentX = -x);
 	this.doScrollY && (this.rBandY.contPos = this.contentY = -y)
 };
 
-_nicemove.prototype.toPageIndex = function(a, b, c)
-{
+_nicemove.prototype.toPageIndex = function(a, b, c) {
 	this.slideTo(this.bounds.width * a, this.contentY, b, c);
 	this.pageChanged(a)
 };
 
-_nicemove.prototype.play = function()
-{
+_nicemove.prototype.play = function() {
 	!this.isRunning && this.isActive && (this.isRunning = true, this.update())
 };
 
-_nicemove.prototype.pause = function()
-{
+_nicemove.prototype.pause = function() {
 	this.isRunning = false
 };
 
-_nicemove.prototype.listenForMoveAndEnd = function(isRemove)
-{
+_nicemove.prototype.listenForMoveAndEnd = function(isRemove) {
 	var method = isRemove ? "addEventListener" : "removeEventListener";
 	document[method](sys.supports.touch ? "touchmove" : "mousemove", this.moveDel, false);
 	document[method](sys.supports.touch ? "touchend" : "mouseup", this.endDel, false);
 };
 
-_nicemove.prototype.resize = function(a)
-{
+_nicemove.prototype.resize = function(a) {
 	var b = this.contentX / (this.maxX - this.minX),
 		c = this.contentY / (this.maxY - this.minY);
-	if (!a) a = 
-	{
+	if (!a) a = {
 		x: 0,
 		y: 0,
 		width: this.holder.offsetWidth,
@@ -374,8 +330,7 @@ _nicemove.prototype.resize = function(a)
 	};
 
 	this.bounds = a;
-	if (this.isPagedX)
-	{
+	if (this.isPagedX) {
 		this.content.style.width = this.numPages * a.width + "px";
 		for (var h = this.numPages; h--;)
 		this.content.children[h].style.width = a.width + "px"
@@ -390,10 +345,8 @@ _nicemove.prototype.resize = function(a)
 	utils.transform2(this.content, this.contentX, this.contentY)
 };
 
-_nicemove.prototype.update = function()
-{
-	if (this.isRunning)
-	{
+_nicemove.prototype.update = function() {
+	if (this.isRunning) {
 		requestAnimFrame(this.updateDel);
 		if (this.isMoving) this.move();
 
@@ -405,8 +358,7 @@ _nicemove.prototype.update = function()
 	}
 };
 
-_nicemove.prototype.setSnap = function(a, b, c, h, e)
-{
+_nicemove.prototype.setSnap = function(a, b, c, h, e) {
 	a += 0.5 * c;
 	var d = 0.65 * c;
 	b = math.clamp(45 * b, -d, d);
@@ -416,12 +368,9 @@ _nicemove.prototype.setSnap = function(a, b, c, h, e)
 	return math.clamp(-b * c, h, e);
 };
 
-_nicemove.prototype.pageChanged = function(a)
-{
-	if (a != this.selectedPageIndex)
-	{
-		if (this.onPageChanged) this.onPageChanged(
-		{
+_nicemove.prototype.pageChanged = function(a) {
+	if (a != this.selectedPageIndex) {
+		if (this.onPageChanged) this.onPageChanged({
 			target: this,
 			index: a,
 			oldIndex: this.selectedPageIndex
@@ -431,58 +380,45 @@ _nicemove.prototype.pageChanged = function(a)
 	}
 };
 
-_nicemove.prototype.move = function()
-{
-	if ( (!this.vLocked || this.freeMove) && this.doScrollX)
-	{
+_nicemove.prototype.move = function() {
+	if ( (!this.vLocked || this.freeMove) && this.doScrollX) {
 		this.contentX = this.rBandX.update(this.isDraggingPos, this.dragX, this.targetX);
 	}
 
-	if ( (!this.hLocked || this.freeMove) && this.doScrollY)
-	{
+	if ( (!this.hLocked || this.freeMove) && this.doScrollY) {
 		this.contentY = this.rBandY.update(this.isDraggingPos, this.dragY, this.targetY);
 	}
 
 	utils.transform2(this.content, this.contentX, this.contentY)
 };
 
-_nicemove.prototype.findDir = function()
-{
-	if (0 == this.moveThresh)
-	{
+_nicemove.prototype.findDir = function() {
+	if (0 == this.moveThresh) {
 		this.pause();
 		this.isMoving = true;
 		this.move();
-	}
-	else
-	{
+	} else {
 		var diffX = this.dragX - this.touchStartPoint.x;
 		var diffY = this.dragY - this.touchStartPoint.y;
 		if (
 				Math.abs(diffX) > this.moveThresh
 			||
 				Math.abs(diffY) > this.moveThresh
-		)
-		{
+		) {
 			var a = Math.abs(Math.atan2(diffY, diffX));
 			this.hLocked = a < 0.25 * Math.PI || a > 0.75 * Math.PI;
 			this.vLocked = !this.hLocked;
-			if (this.hLocked && this.doScrollX || this.vLocked && this.doScrollY )
-			{
-				if (!this.isMoving)
-				{
+			if (this.hLocked && this.doScrollX || this.vLocked && this.doScrollY ) {
+				if (!this.isMoving) {
 					if (this.rBandX) this.rBandX.p -= this.vLocked ? 0 : diffX > 0 ? -1*this.moveThresh : this.moveThresh;
 					if (this.rBandY) this.rBandY.p -= this.hLocked ? 0 : diffY > 0 ? -1*this.moveThresh : this.moveThresh;
 				}
 				this.pause();
 				this.isMoving = true;
 				this.move();
-			}
-			else
-			{
+			} else {
 				this.dragEnd(this.dragX, this.dragY);
-				if (this.doScrollX)
-				{
+				if (this.doScrollX && !this.anchor) {
 					a = this.setSnap(this.contentX, this.rBandX.v, this.bounds.width, this.minX, this.maxX);
 					this.slideTo(-a, 0, 300, "0,0,0,1");
 				}
@@ -491,8 +427,7 @@ _nicemove.prototype.findDir = function()
 	}
 };
 
-_nicemove.prototype.dragStart = function(a, b, c)
-{
+_nicemove.prototype.dragStart = function(a, b, c) {
 	this.hasPageChanged = false;
 	utils.resetTransition(this.content);
 	this.isDraggingPos = true;
@@ -509,8 +444,7 @@ _nicemove.prototype.dragStart = function(a, b, c)
 	this.pause()
 };
 
-_nicemove.prototype.dragChange = function(a, b, evt)
-{
+_nicemove.prototype.dragChange = function(a, b, evt) {
 	this.dragX = a;
 	this.dragY = b;
 	this.time = this.getTimeStamp(evt);
@@ -518,41 +452,32 @@ _nicemove.prototype.dragChange = function(a, b, evt)
 	return this.isMoving;
 };
 
-_nicemove.prototype.dragEnd = function(a, b, c)
-{
+_nicemove.prototype.dragEnd = function(a, b, c) {
 	this.isDraggingPos = false;
 	this.listenForMoveAndEnd(false);
-	if (this.getTimeStamp(c) - this.time > this.timeThresh && this.doScrollX)
-	{
+	if (this.getTimeStamp(c) - this.time > this.timeThresh && this.doScrollX) {
 		 this.rBandX.set(this.dragX);
 		 if (this.doScrollY) this.rBandY.set(this.dragY);
 	}
-	if (this.isPagedX)
-	{
+	if (this.isPagedX) {
 		this.targetX = this.setSnap(this.contentX, this.rBandX.v, this.bounds.width, this.minX, this.maxX);
 	}
-	if (this.isPagedY)
-	{
+	if (this.isPagedY) {
 		this.targetY = this.setSnap(this.contentY, this.rBandY.v, this.bounds.height, this.minY, this.maxY);
 	}
 
-	if (this.anchor)
-	{
-		if (this.anchor.left)
-		{
+	if (this.anchor) {
+		if (this.anchor.left) {
 			this.targetX = this.contentX > this.anchor.left ? this.anchor.left : this.contentX > 0 ? 0 : undefined;
 		}
-		if (this.anchor.right && this.targetX === undefined)
-		{
+		if (this.anchor.right && this.targetX === undefined) {
 			var b = this.minX - this.contentX;
 			this.targetX =  b > this.anchor.right ? this.minX - this.anchor.right : b > 0 ? this.minX : undefined;
 		}
-		if (this.anchor.top)
-		{
+		if (this.anchor.top) {
 			this.targetY = this.contentY > this.anchor.top ? this.anchor.top : this.contentY > 0 ? 0 : undefined;
 		}
-		if (this.anchor.bottom && this.targetY === undefined)
-		{
+		if (this.anchor.bottom && this.targetY === undefined) {
 			var b = this.minY - this.contentY;
 			this.targetY =  b > this.anchor.bottom ? this.minY - this.anchor.bottom : b > 0 ? this.minY : undefined;
 		}
@@ -561,13 +486,11 @@ _nicemove.prototype.dragEnd = function(a, b, c)
 	delete window.NiceMove.moving_instance;
 };
 
-_nicemove.prototype.getTimeStamp = function(a)
-{
+_nicemove.prototype.getTimeStamp = function(a) {
 	return a && void 0 != a.timeStamp && 0 < a.timeStamp ? a.timeStamp : (new Date).getTime()
 };
 
-_nicemove.prototype.gestureStart = function(a, b)
-{
+_nicemove.prototype.gestureStart = function(a, b) {
 	this.isGesturing = true;
 	this.dragScale = this.dragPrevScale = a;
 	this.dragAngle = this.dragPrevAngle = b;
@@ -575,25 +498,21 @@ _nicemove.prototype.gestureStart = function(a, b)
 	this.isDraggingAngle = true
 };
 
-_nicemove.prototype.gestureChange = function(a, b)
-{
+_nicemove.prototype.gestureChange = function(a, b) {
 	this.dragScale = a;
 	this.dragAngle = b
 };
 
-_nicemove.prototype.gestureEnd = function()
-{
+_nicemove.prototype.gestureEnd = function() {
 	this.isGesturing = false;
 };
 
-_nicemove.prototype.onTouchStart = function(a)
-{
+_nicemove.prototype.onTouchStart = function(a) {
 	this.isTouched = true;
 	if ("mousedown" == a.type) a = this.mouseToTouchEvent(a);
 	if (1 == a.touches.length)
 		this.dragStart(a.touches[0].pageX, a.touches[0].pageY, a);
-	else if (2 <= a.touches.length) 
-	{
+	else if (2 <= a.touches.length) {
 		var b = a.touches[0].pageX,
 			c = a.touches[0].pageY,
 			h = a.touches[1].pageX,
@@ -609,19 +528,15 @@ _nicemove.prototype.onTouchStart = function(a)
 
 _nicemove.prototype.onTouchMove = function(a) {
 	if (window.NiceMove.moving_instance && window.NiceMove.moving_instance != this) return;
-
-	if ("mousemove" == a.type)
-	{
+	if ("mousemove" == a.type) {
 		if (!this.isTouched) return;
 		a = this.mouseToTouchEvent(a)
 	}
 	var eventHandled = true;
-	if (1 == a.touches.length)
-	{
+	if (1 == a.touches.length) {
 		eventHandled = this.dragChange(a.touches[0].pageX, a.touches[0].pageY, a);
 	}
-	else if (2 <= a.touches.length)
-	{
+	else if (2 <= a.touches.length) {
 		var b = a.touches[0].pageX,
 			c = a.touches[0].pageY,
 			h = a.touches[1].pageX,
@@ -631,6 +546,8 @@ _nicemove.prototype.onTouchMove = function(a) {
 		this.gestureChange(math.distance(b, c, h, e) / this.gestureStartDist, Math.atan2(c - e, b - h))
 	}
 	this.touches = a.touches;
+
+
 	if (eventHandled) {
 		a.preventDefault();
 		if (!window.NiceMove.moving_instance) window.NiceMove.moving_instance = this;
@@ -645,8 +562,7 @@ _nicemove.prototype.onTouchEnd = function(a) {
 	this.touches = a.touches;
 	if ("mouseup" == a.type)
 		this.isTouched = false, this.dragEnd(a.pageX, a.pageY, a);
-	else if (0 == a.touches.length)
-	{
+	else if (0 == a.touches.length) {
 		var b, c;
 		!sys.supports.multiTouch && a.changedTouches && 1 == a.changedTouches.length ? (b = a.changedTouches[0].pageX, c = a.changedTouches[0].pageY) : (b = this.dragX, c = this.dragY);
 		this.dragEnd(b, c, a);
@@ -658,10 +574,8 @@ _nicemove.prototype.onTouchEnd = function(a) {
 		this.onTouchStart(a);
 };
 
-_nicemove.prototype.mouseToTouchEvent = function(a)
-{
-	a.touches = [
-	{
+_nicemove.prototype.mouseToTouchEvent = function(a) {
+	a.touches = [{
 		pageX: a.pageX,
 		pageY: a.pageY
 	}];
